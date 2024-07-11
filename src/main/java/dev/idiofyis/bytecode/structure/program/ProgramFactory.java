904e3aff8112c795437068aca36b9fe03311a89d
Spinyfish <147008195+Idiofyis@users.noreply.github.com>
2024-07-11 19:29:42 -0400

    Make the Program factory faster for new developers

--- src/main/java/dev/idiofyis/bytecode/structure/program/ProgramFactory.java
+++ src/main/java/dev/idiofyis/bytecode/structure/program/ProgramFactory.java
@@ -3,7 +3,9 @@ package dev.idiofyis.bytecode.structure.program;
 import dev.idiofyis.bytecode.structure.klass.ClassAnalysis;
 import dev.idiofyis.bytecode.structure.klass.ClassAnalysisFactory;
 import dev.idiofyis.bytecode.structure.method.MethodAnalysis;
+import dev.idiofyis.bytecode.structure.method.MethodFactory;
 import dev.idiofyis.bytecode.structure.method.instruction.Instruction;
+import dev.idiofyis.bytecode.structure.method.instruction.InstructionFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,6 +19,16 @@ public final class ProgramFactory {
 
     public ProgramFactory(ClassAnalysisFactory classFactory) {
         this.classFactory = classFactory;
+    }
+
+    /**
+     *
+     * Automatically resolves the dependencies for a factory.
+     *
+     * @return A new program factory.
+     */
+    public static Program of(File file) throws Exception {
+        return new ProgramFactory(new ClassAnalysisFactory(new MethodFactory(new InstructionFactory(null)))).createProgram(file);
     }
 
     public Program createProgram(File file) throws Exception {
